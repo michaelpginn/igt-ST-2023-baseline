fc821ef8d779ca120a31fbbda13f23fa03ef3f8e
michaelpginn <milespergallon271@gmail.com>
2023-02-17 21:55:38 -0600

    Add debug

--- src/model.py
+++ src/model.py
@@ -45,9 +45,9 @@ def create_trainer(model: BartForConditionalGeneration, dataset, encoder: MultiV
         decoded_preds = encoder.batch_decode(preds)
 
         # Decode (gold) labels
-        print(labels)
         labels = np.where(labels != -100, labels, encoder.PAD_ID)
         decoded_labels = encoder.batch_decode(labels)
+        print(decoded_labels)
         return eval_morpheme_glosses(pred_morphemes=decoded_preds, gold_morphemes=decoded_labels)
 
     args = Seq2SeqTrainingArguments(
